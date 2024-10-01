e496bf02ea30924bdda9b7979645c737819c2b3d
Fidelis-Muteti <fidelmwovi@gmail.com>
2024-10-01 12:15:35 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+ let num1=2
+let num2=31
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 10;
+let num4 = 6; 
+let mod = num3 % num4;
+
+console.log(mod);
+
+let numbers = [10, 5, 20, 8, 15];
+
+let max = Math.max(...numbers);
+
+console.log(max); 
